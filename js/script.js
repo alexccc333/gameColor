d80b6c9107fdf26ae5da6e0bd22959aa27353860
alex <constintmid@mail.ru>
2019-05-21 09:48:49 +0200

    add fix js

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ var id1;
 var id2;
 var blockColor;
 let m=0;
+let countColor=0;
 var gameColor= [
         'class="blue game-block" data-i="1"',
         'class="yellow game-block" data-i="2"',
@@ -86,7 +87,16 @@ function shuffle(array) {
 
                  document.querySelector(`.game-block[data-x="${pos1}"][data-y="${pos2}"]`).classList.remove('white');
                  document.querySelector(`.game-block[data-x="${this.id1}"][data-y="${this.id2}"]`).classList.remove('white');
+                 countColor++;
+                 console.log(countColor);
+                 if (countColor==8)
+                 {
+                     setTimeout(()=>{
+                         alert("end game"+readout);
+                     },1000);
 
+
+                 }
          }
      else {
              setTimeout(function () {
@@ -99,111 +109,109 @@ function shuffle(array) {
      }
 
  }
- draw()
-//  function gameStart(){
-//      StartStop();
-//      draw();
-//  }
-//
-// //объявляем переменные
-// var base = 60;
-// var clocktimer, dateObj, dh, dm, ds, ms;
-// var readout = '';
-// var h = 1,
-//     m = 1,
-//     tm = 1,
-//     s = 0,
-//     ts = 0,
-//     ms = 0,
-//     init = 0;
-//
-// //функция для очистки поля
-// function ClearСlock() {
-//     clearTimeout(clocktimer);
-//     h = 1;
-//     m = 1;
-//     tm = 1;
-//     s = 0;
-//     ts = 0;
-//     ms = 0;
-//     init = 0;
-//     readout = '00:00:00';
-//     document.MyForm.stopwatch.value = readout;
-// }
-//
-// //функция для старта секундомера
-// function StartTIME() {
-//     var cdateObj = new Date();
-//     var t = (cdateObj.getTime() - dateObj.getTime()) - (s * 1000);
-//     if (t > 999) {
-//         s++;
-//     }
-//     if (s >= (m * base)) {
-//         ts = 0;
-//         m++;
-//     } else {
-//         ts = parseInt((ms / 100) + s);
-//         if (ts >= base) {
-//             ts = ts - ((m - 1) * base);
-//         }
-//     }
-//     if (m > (h * base)) {
-//         tm = 1;
-//         h++;
-//     } else {
-//         tm = parseInt((ms / 100) + m);
-//         if (tm >= base) {
-//             tm = tm - ((h - 1) * base);
-//         }
-//     }
-//     ms = Math.round(t / 10);
-//     if (ms > 99) {
-//         ms = 0;
-//     }
-//     if (ms == 0) {
-//         ms = '00';
-//     }
-//     if (ms > 0 && ms <= 9) {
-//         ms = '0' + ms;
-//     }
-//     if (ts > 0) {
-//         ds = ts;
-//         if (ts < 10) {
-//             ds = '0' + ts;
-//         }
-//     } else {
-//         ds = '00';
-//     }
-//     dm = tm - 1;
-//     if (dm > 0) {
-//         if (dm < 10) {
-//             dm = '0' + dm;
-//         }
-//     } else {
-//         dm = '00';
-//     }
-//     dh = h - 1;
-//     if (dh > 0) {
-//         if (dh < 10) {
-//             dh = '0' + dh;
-//         }
-//     } else {
-//         dh = '00';
-//     }
-//     readout = dh + ':' + dm + ':' + ds;
-//     document.MyForm.stopwatch.value = readout;
-//     clocktimer = setTimeout("StartTIME()", 1);
-// }
-//
-// //Функция запуска и остановки
-// function StartStop() {
-//     if (init == 0) {
-//         ClearСlock();
-//         dateObj = new Date();
-//         StartTIME();
-//         init = 1;
-//     } else {
-//         clearTimeout(clocktimer);
-//         init = 0;
-//     }
-// }
+ function GamingStart(){
+     StartStop();
+     draw();
+ }
+
+//объявляем переменные
+var base = 60;
+var clocktimer, dateObj, dh, dm, ds, ms ,md;
+var readout = '';
+var h = 1,
+    md = 1,
+    tm = 1,
+    s = 0,
+    ts = 0,
+    ms = 0,
+    init = 0;
+//функция для очистки поля
+function ClearСlock() {
+    clearTimeout(clocktimer);
+    h = 1;
+    md = 1;
+    tm = 1;
+    s = 0;
+    ts = 0;
+    ms = 0;
+    init = 0;
+    readout = '00:00:00';
+    document.MyForm.stopwatch.value = readout;
+}
+
+//функция для старта секундомера
+function StartTIME() {
+    var cdateObj = new Date();
+    var t = (cdateObj.getTime() - dateObj.getTime()) - (s * 1000);
+    if (t > 999) {
+        s++;
+    }
+    if (s >= (md * base)) {
+        ts = 0;
+        md++;
+    } else {
+        ts = parseInt((ms / 100) + s);
+        if (ts >= base) {
+            ts = ts - ((md - 1) * base);
+        }
+    }
+    if (md > (h * base)) {
+        tm = 1;
+        h++;
+    } else {
+        tm = parseInt((ms / 100) + md);
+        if (tm >= base) {
+            tm = tm - ((h - 1) * base);
+        }
+    }
+    ms = Math.round(t / 10);
+    if (ms > 99) {
+        ms = 0;
+    }
+    if (ms == 0) {
+        ms = '00';
+    }
+    if (ms > 0 && ms <= 9) {
+        ms = '0' + ms;
+    }
+    if (ts > 0) {
+        ds = ts;
+        if (ts < 10) {
+            ds = '0' + ts;
+        }
+    } else {
+        ds = '00';
+    }
+    dm = tm - 1;
+    if (dm > 0) {
+        if (dm < 10) {
+            dm = '0' + dm;
+        }
+    } else {
+        dm = '00';
+    }
+    dh = h - 1;
+    if (dh > 0) {
+        if (dh < 10) {
+            dh = '0' + dh;
+        }
+    } else {
+        dh = '00';
+    }
+    readout = dm + ':' + ds + ':' + ms;
+    document.MyForm.stopwatch.value = readout;
+    clocktimer = setTimeout("StartTIME()", 1);
+}
+
+//Функция запуска и остановки
+function StartStop() {
+    if (init == 0) {
+        ClearСlock();
+        dateObj = new Date();
+        StartTIME();
+        init = 1;
+    } else {
+        clearTimeout(clocktimer);
+        init = 0;
+    }
+}
